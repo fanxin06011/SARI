c14fc215d212893b6962acc349cd9f6d689e86f3
liucanpkucn <can.liu@pku.edu.cn>
2020-02-15 20:52:25 +0800

    a

---
+++ client/js/data.js
@@ -0,0 +1,285 @@
+let get_modify_data = function(data){
+  window._data = data
+  // accumulate_data_liucan()
+  // console.log("old", ncov_data)
+  let data_contain = convert_data_liucan(data, "全国")
+  let ncov_data_new = data_contain.table_data;
+  let minus_ncov_data = data_contain.minus_table_data;
+  let begin_date = data_contain.begin_date;
+  // console.log("minus_table_data", minus_ncov_data)
+  // console.log("old ncov", ncov_data)
+  let ncov_data_accu = accumulate_data_liucan(ncov_data_new, minus_ncov_data)
+  ncov_data_new = modify_data_liucan(ncov_data_new);
+  ncov_data_accu = modify_data_liucan(ncov_data_accu);
+  minus_ncov_data = modify_data_liucan(minus_ncov_data);
+  return {new: ncov_data_new, minus: minus_ncov_data, accu: ncov_data_accu, begin:begin_date}
+}
+let modify_data_liucan = function(data){
+  let new_data = new Array();
+  let data_length = data.length;
+  for (let i = 0; i < data_length; i ++ )
+  {
+    new_data[data[i][0]] = data[i].slice(1, data[i].length)
+  }
+  return new_data
+}
+
+let accumulate_data_liucan = function(ncov_data_input, ncov_data_minus)
+{
+  let ncov_data_tmp = new Array()
+
+  for (let i = 0; i < ncov_data_input.length; i ++ )
+  {
+    ncov_data_tmp[i] = new Array()
+    ncov_data_tmp[i][0] = ncov_data_input[i][0]
+    ncov_data_tmp[i][1] = ncov_data_input[i][1] + ncov_data_minus[i][1]
+    
+    // ncov_data[i][0] = modify_name(ncov_data[i][0])
+    for (let j = 2; j < ncov_data_input[i].length; j ++ )
+      ncov_data_tmp[i][j] = ncov_data_tmp[i][j - 1] + ncov_data_input[i][j] + ncov_data_minus[i][j]
+  }
+  ncov_data_new = ncov_data_input
+  ncov_data_accu = ncov_data_tmp
+  return ncov_data_accu
+
+  // return ncov_data_tmp
+}
+
+let convert_data_liucan  = function(data, simple_province_name){
+    let cities = ["新疆", "西藏", "内蒙古", "青海", "四川", "黑龙江", "甘肃", "云南", "广西", "湖南", "陕西", "广东", "吉林", "河北", "湖北", "贵州", "山东", "江西", "河南", "辽宁", "山西", "安徽", "福建", "浙江", "江苏", "重庆", "宁夏", "海南", "台湾", "北京", "天津", "上海", "香港", "澳门"]
+    let provinces = cities
+    // console.log(data)
+    let item_number = data.length
+    let new_data = new Array()
+    let date_list = new Array()
+    let minus_data = new Array()
+
+    for (i = 0; i < item_number; i ++)
+    {
+      let current_item = data[i]
+      let city_name
+      if (simple_province_name === "全国")
+      {
+        if (current_item["类别"] === "国家级"){
+          city_name = "total"
+        }
+        else if (current_item["类别"] != "省级")
+          continue
+        else 
+          city_name = current_item["省份"].replace("市", "").replace("恩施州", "恩施").replace("林区", "")
+      }
+      else {
+        if (current_item["类别"] === "省级" && current_item["省份"] === simple_province_name){
+          city_name = "total"
+          if (current_item["新增确诊病例"] === "")
+            continue
+        }
+        else if (current_item["类别"] != "地区级")
+          continue
+        else if (current_item["省份"] != simple_province_name && current_item["省份"] != full_province_name_dict[simple_province_name])
+          continue
+        else
+          city_name = current_item["城市"].replace("市", "").replace("恩施州", "恩施").replace("林区", "")
+      }
+      // city_name = correct_simple_name(city_name)
+      // console.log(city_name)
+      if (!new_data.hasOwnProperty(city_name))
+        new_data[city_name] = new Array()
+
+      if (!minus_data.hasOwnProperty(city_name))
+        minus_data[city_name] = new Array()
+
+      let new_add = current_item["新增确诊病例"]
+      new_add = parse_to_int_liucan(new_add)
+
+      let new_minus = current_item["核减"]
+      new_minus = parse_to_int_liucan(new_minus)
+      // if (new_minus !== 0)
+      //   console.log(city_name+ current_item["公开时间"] +  " 核减 " + new_minus )
+
+      minus_data[city_name][current_item["公开时间"]] = new_minus
+
+      if (!new_data[city_name].hasOwnProperty(current_item["公开时间"])){
+        new_data[city_name][current_item["公开时间"]] = new_add
+      }
+      else {
+        new_data[city_name][current_item["公开时间"]] += new_add
+        // if (parseInt(current_item["新增确诊病例"]) > new_data[city_name][current_item["公开时间"]])
+        //   new_data[city_name][current_item["公开时间"]] = parseInt(current_item["新增确诊病例"])
+      }
+      if (date_list.indexOf(get_day_liucan_index(current_item["公开时间"])) === -1)
+        date_list.push(get_day_liucan_index(current_item["公开时间"]))
+    }
+    // console.log("new_data ", new_data)
+    // console.log("minus data", minus_data)
+    date_list = date_list.sort()
+    // console.log(date_list)
+    let begin_date = date_list[0]
+    let end_date = date_list[date_list.length - 1]
+    // console.log("begin_date", begin_date)
+    // console.log("end_date", end_date)
+    diff_day = end_date - begin_date
+    // console.log(diff_day)
+
+    let table_data = new Array()
+    let minus_table_data = new Array()
+    let city_list = Object.keys(new_data)
+    let city_dict = find_pair_liucan(city_list)
+    // console.log("city_dict", city_dict)
+    let used_city = new Array()
+    for (i = 0; i < city_list.length; i ++ )
+    {
+      minus_table_data[i] = new Array()
+      minus_table_data[i][0] = city_dict[city_list[i]]
+
+      table_data[i] = new Array()
+      table_data[i][0] = city_dict[city_list[i]]
+
+      used_city[i] = city_dict[city_list[i]] 
+
+      for (j = 0; j < diff_day + 1; j ++)
+      {
+        // console.log(city_list)
+        // console.log(city_list[i])
+        // console.log()
+        if (minus_data[city_list[i]].hasOwnProperty(get_day_liucan(j + 1, begin_date))){
+          // console.log(minus_data[city_list[i]][get_day_liucan(j + 1)])
+          minus_table_data[i][j + 1] = minus_data[city_list[i]][get_day_liucan(j + 1, begin_date)]
+        }
+        else
+          minus_table_data[i][j + 1] = 0
+
+        if (new_data[city_list[i]].hasOwnProperty(get_day_liucan(j + 1, begin_date)))
+          table_data[i][j + 1] = new_data[city_list[i]][get_day_liucan(j + 1, begin_date)]
+        else table_data[i][j + 1] = 0
+      }
+      // for (j = 1; j < )
+    }
+
+    // console.log("old table_data"+ table_data)
+    // console.log("minus_table_data", minus_table_data)
+    // console.log("city_list", city_list)
+    // console.log("used_city", used_city)
+    // console.log("cities", cities)
+    let city_index = city_list.length
+    for (i = 0; i < cities.length; i ++)
+    {
+      // console.log("正在排查", provinces[i])
+      if (used_city.indexOf(provinces[i]) === -1)
+      {
+        // console.log(provinces[i])
+        table_data[city_index] = new Array()
+        // console.log(table_data[city_index])
+        // table_data[city_index][2] = 0
+        minus_table_data[city_index] = new Array()
+
+        table_data[city_index][0] = provinces[i]
+        minus_table_data[city_index][0] = provinces[i]
+        for (j = 0; j < diff_day + 1; j ++)
+        {
+          // console.log(city_list)
+          // console.log(city_list[i])
+          // console.log("asdf" + table_data[city_index])
+          minus_table_data[city_index][j + 1] = 0
+          table_data[city_index][j + 1] = 0
+          // console.log(city_index)
+        }
+        city_index = city_index + 1
+      }
+    }
+    // console.log("new table_data" + table_data)
+    let table_data_length = table_data.length
+    // console.log("table_data_length", table_data_length)
+    // for (i = 0; i < table_data_length; i ++ )
+    // {
+    //   table_data[i][0] = correct_simple_name(table_data[i][0])
+    // }
+    
+
+    // console.log(city_list)
+    // console.log("new table_data", table_data)
+    return {table_data: table_data, minus_table_data: minus_table_data, begin_date: begin_date}
+}
+
+function find_pair_liucan(city_list){
+  cities = ["新疆", "西藏", "内蒙古", "青海", "四川", "黑龙江", "甘肃", "云南", "广西", "湖南", "陕西", "广东", "吉林", "河北", "湖北", "贵州", "山东", "江西", "河南", "辽宁", "山西", "安徽", "福建", "浙江", "江苏", "重庆", "宁夏", "海南", "台湾", "北京", "天津", "上海", "香港", "澳门"]
+  // console.log("city_list", city_list)
+  // console.log("cities", cities)
+  let city_dict = new Array()
+  for (let i = 0; i < city_list.length; i ++)
+  {
+    city_dict[city_list[i]] = city_list[i]
+    for (let j = 0; j < cities.length; j ++)
+    {
+      if (city_list[i].indexOf(cities[j]) >= 0)
+      {
+        city_dict[city_list[i]] = cities[j]
+      }
+    }
+  }
+  // console.log(city_dict)
+  return city_dict
+}
+
+function parse_to_int_liucan(input){
+  if (input === "")
+    return 0
+  return parseInt(input)
+}
+function get_day_liucan_index(day_str)
+{
+  // console.log(day_str)
+  let date_array = day_str.split(/月|日/)
+  let month = parseInt(date_array[0])
+  let day = parseInt(date_array[1])
+  if (month == 1)
+    return day
+  if (month == 2)
+    return day + 31
+  if (month == 3)
+    return day + 60
+  return (month - 1) * 30 + day 
+
+
+  // let date = i + 10
+  // if (date > 31)
+  // {
+  //   return "2月" + (date - 31) + "日"
+  // }
+  // return "1月" + date + "日"
+  return 0
+}
+
+function get_day_liucan(i, begin_date)
+{
+  let date = i + begin_date - 1
+  if (date > 31)
+  {
+    return "2月" + (date - 31) + "日"
+  }
+  return "1月" + date + "日"
+}
+
+function get_day_en(i, begin_date)
+{
+  let date = i + begin_date - 1
+  if (date > 31)
+  {
+    return "Feb. " + (date - 31) 
+  }
+  else if (date > 60)
+  {
+    return "Mar. " + (date - 60) 
+  }
+  else if (date > 91)
+  {
+    return "Apr. " + (date - 91)
+  }
+  else if (date > 121)
+  {
+    return "May " + (date - 91)
+  }
+  return "Jan. " + date 
+}
+
+
---
+++ client/js/data_chooce.js
@@ -0,0 +1,389 @@
+let DataPanel = function(data, provinces){
+	this.data = data;
+	this.provinces = provinces
+	this.place_is_choose = new Array();
+	this.provinces_num = provinces.length;
+	for (let i = 0; i < this.provinces_num; i ++){
+		this.place_is_choose[provinces[i]] = true
+	}
+	// this.place_is_choose["山东"] = 
+	this.place_is_choose["全国"] = true
+	this.place_is_choose["仅湖北"] = false
+	this.place_is_choose['除湖北'] = false
+
+	this.margin = {left: 0, right: 0, top: 0, bottom: 0};
+	this.width = document.getElementById('middle-div').clientWidth - this.margin.left - this.margin.right,
+    this.height = document.getElementById('middle-div').clientHeight - this.margin.top - this.margin.bottom;
+    this.svg = d3.select("#middle-div").append("svg")
+	    .attr("id", "panel_svg")
+	    .attr("font-family", "Arial")
+	    .attr("width", this.width - this.margin.left - this.margin.right)
+	    .attr("height", this.height - this.margin.top - this.margin.bottom);
+
+	// this.svg.append("rect")
+	// 	.attr("width", this.width)
+	// 	.attr("height", this.height)
+
+	this.title_position = {left: 0, top: 0, width: this.width, height: this.height * 0.05};
+	this.time_position = {left: this.width * 0.05, top: this.height * 0.05, width: this.width * 0.9, height: this.height * 0.42};
+	this.area_position = {left: 0, top: this.height * 0.5, width: this.width, height: this.height * 0.5};
+	
+	// this.load_title(this.svg, this.title_position)
+}
+DataPanel.prototype.load_title = function(){
+	let svg = this.svg
+	let title_position = this.title_position
+	this.title = svg.append("g")
+		.attr("transform", function(d){
+			return "translate(" + title_position.left + "," + title_position.top + ")"
+		})
+	this.title.append("text")
+		.attr("x", title_position.width / 2)
+		.attr("y", title_position.height * 0.8 )
+		.style("font-size", title_position.width / 10)
+		.attr("text-anchor", "middle")
+		.text("数据选择")
+
+}
+// DataPanel.prototype.load_place_color = function(){
+
+// }
+
+DataPanel.prototype.load_time = function(){
+	let svg = this.svg
+	let data = this.data
+	let time_position = this.time_position
+	let width = time_position.width
+	let height = time_position.height
+	let left = time_position.left
+	let top = time_position.top
+	let data_item_number = this.data.new.total.length
+	this.data_item_number = data_item_number
+	let left_date_text
+	let right_date_text
+	this.date_range = new Array()
+	let date_range = this.date_range
+	date_range.left = 0
+	date_range.right = data_item_number - 1
+
+	let line = d3.line()
+		.x(function(d, i) { return width * i / data_item_number; }) // set the x values for the line generator
+		.y(function(d) { return y_scale(d); }) // set the y values for the line generator 
+		.curve(d3.curveMonotoneX) // apply smoothing to the line
+
+	let new_data = this.get_data(this.data.new)
+	let accu_data = this.get_data(this.data.accu)
+	this.new_data = new_data
+	this.accu_data = accu_data
+	console.log(new_data)
+	console.log(accu_data)
+
+
+	this.y_scale = d3.scaleLinear()
+    	.domain([0, Math.max(...accu_data)])
+    	.range([height, 0]);
+
+    this.x_scale = d3.scaleLinear()
+    	.domain([0, data_item_number])
+    	.range([0, width])
+
+
+
+    let y_scale = this.y_scale
+    let x_scale = this.x_scale
+
+    window._y_scale = this.y_scale
+    window._x_scale = this.x_scale
+
+	this.time = svg.append("g")
+		.attr("id", "time_area")
+		.attr("transform", function(d){
+			return "translate(" + left + "," + top + ")"
+		})
+	// console.log(new_data)
+	this.new_rect = this.time.append("g")
+
+	this.new_rect.selectAll(".new_rect")
+		.data(new_data)
+		.enter()
+		.append("rect")
+		.attr("class", "new_rect")
+		.attr("height", d => (y_scale(0) - y_scale(d)))
+		.attr("y", d => y_scale(d))
+		.attr("x", (d,i) => x_scale(i))
+		.attr("fill", "#ccebc5")
+		.attr("width", width / data_item_number)
+
+	this.accu_path = this.time.append("path")
+	    .datum(accu_data) // 10. Binds data to the line 
+	    .attr("class", "line") // Assign a class for styling 
+	    .attr("id", "accu_path")
+	    .attr("d", line) // 11. Calls the line generator 
+	    .attr("fill-opacity", 0)
+	    .attr("stroke-width", 2)
+	    .attr("stroke", "#b3cde3");
+
+	this.time.append('g')
+		.attr("class", "y-axis")
+		.call(d3.axisRight(y_scale)); // Create an axis component with d3.axisLeft
+
+	var brush = d3.brushX()
+	    .extent([[0, 0], [width, height]])
+	    .on("brush end", function(d){
+	    	let selection = d3.event.selection
+			let left_date = Math.floor(x_scale.invert(selection[0]))
+			let right_date = Math.floor(x_scale.invert(selection[1]))
+			if (right_date >= data_item_number)
+				right_date = data_item_number - 1
+			date_range.left = left_date
+			date_range.right = right_date
+
+			left_date_text
+				.text(get_day_en(left_date, data.begin))
+				.attr("x", selection[0])
+			right_date_text
+				.text(get_day_en(right_date, data.begin))
+				.attr("x", selection[1])
+	    });
+
+	this.time.append("g")
+		.attr("class", "brush")
+		.call(brush)
+
+	let show_date = this.time.append("g")
+		.attr("id", "show_date")
+		.attr("transform", "translate(0," + height * 1.02 + ")")
+
+	left_date_text = show_date.append("text")
+		.attr("class", "left_date")
+		.text(get_day_en(0, this.data.begin))
+		.attr("dominant-baseline", "hanging")
+		.attr("text-anchor", "middle")
+
+	right_date_text = show_date.append("text")
+		.attr("class", "right_date")
+		.attr("x", width)
+		.text(get_day_en(data_item_number - 1, this.data.begin))
+		.attr("dominant-baseline", "hanging")
+		.attr("text-anchor", "middle")
+
+	this.left_date_text = left_date_text
+	this.right_date_text = right_date_text
+
+	
+}
+
+DataPanel.prototype.reload_time = function(){
+	let height = this.time_position.height
+	let new_data = this.get_data(this.data.new)
+	let accu_data = this.get_data(this.data.accu)
+
+	let line = d3.line()
+		.x(function(d, i) { return x_scale(i); }) // set the x values for the line generator
+		.y(function(d) { return y_scale(d); }) // set the y values for the line generator 
+		.curve(d3.curveMonotoneX) // apply smoothing to the line
+
+	console.log(new_data)
+	console.log(accu_data)
+	this.new_data = new_data
+	this.accu_data = accu_data
+	this.y_scale = d3.scaleLinear()
+    	.domain([0, Math.max(...accu_data)])
+    	.range([height, 0]);
+    let x_scale = this.x_scale
+    let y_scale = this.y_scale
+
+    d3.select(".y-axis")
+    	.call(d3.axisRight(this.y_scale))
+
+    this.accu_path
+    	.datum(accu_data)
+    	.attr("d", line)
+
+    this.new_rect.selectAll(".new_rect")
+    	.attr("height", (d,i) => (y_scale(0) - y_scale(new_data[i])))
+		.attr("y", (d,i) => y_scale(new_data[i]))
+
+}
+
+
+DataPanel.prototype.get_data = function(input_data){
+	let output_data = new Array()
+	console.log(this.data_item_number)
+	for (let i = 0; i < this.data_item_number; i ++ ){
+		output_data[i] = 0
+	}
+	for (let p_i = 0; p_i < this.provinces_num; p_i ++ ){
+		let current_province = this.provinces[p_i]
+		if (this.place_is_choose[current_province]){
+			for (let i = 0; i < this.data_item_number; i ++)
+				output_data[i] += input_data[current_province][i]
+		}
+	}
+	return output_data
+}
+
+
+DataPanel.prototype.load_range = function(places, column_max = 3){
+	let svg = this.svg
+	let position = this.area_position
+	let provinces = this.provinces
+
+	console.log(places)
+	let width = position.width
+	let height = position.height
+	let left = position.left
+	let place_is_choose = this.place_is_choose
+	let top = position.top
+	let button_width = width / column_max
+	let button_height = height / (Math.ceil(places.length / column_max))
+	let button_margin = {left: button_width * 0.1, right: button_width * 0.1, top: button_height * 0.1 , bottom:  button_height * 0.1};
+	
+
+
+	this.area = svg.append("g")
+		.attr("id", "range_area")
+		.attr("transform", function(d){
+			return "translate(" + left + "," + top + ")"
+		})
+
+	// this.area.append("rect")
+	// 	.attr("fill", "blue")
+	// 	.attr("width", width)
+	// 	.attr('height', height)
+	this.province_button = this.area.selectAll(".province")
+		.data(places)
+		.enter()
+		.append("g")
+		.attr("class", "province_button")
+		.classed("selected_area_button", d => place_is_choose[d])
+		.attr("transform", function(d, i){
+			row = parseInt(i / column_max)
+			col = i - row * column_max
+			return "translate(" + col * button_width + "," + row * button_height + ")"
+		})
+
+	let province_button = this.province_button
+
+	this.province_button.append('rect')
+		.attr("width", button_width - button_margin.left - button_margin.right)
+		.attr("height", button_height - button_margin.top - button_margin.bottom)
+		.attr("x", button_margin.left)
+		.attr("y", button_margin.top)
+		.attr("rx", button_margin.left)
+
+	this.province_button.append("text")
+		.text(d => d)
+		.attr("x", button_width / 2)
+		.attr("y", button_height / 2)
+		.attr("text-anchor", "middle")
+		.attr("dominant-baseline", "middle") // hanging
+
+	let panel = this
+
+	this.province_button
+		.on("click", function(d){
+			if (d === "全国")
+			{
+				place_is_choose[d] = true
+				place_is_choose["仅湖北"] = false
+				place_is_choose["除湖北"] = false
+				for (let i = 0; i < provinces.length; i ++)
+				{
+					place_is_choose[provinces[i]] = true
+				}
+			}
+			else if (d === "仅湖北")
+			{
+				place_is_choose[d] = true
+				place_is_choose["全国"] = false
+				place_is_choose["除湖北"] = false
+				for (let i = 0; i < provinces.length; i ++)
+				{
+					place_is_choose[provinces[i]] = false
+				}
+				place_is_choose["湖北"] = true
+			}
+			else if (d === "除湖北")
+			{
+				place_is_choose[d] = true
+				place_is_choose["全国"] = false
+				place_is_choose["仅湖北"] = false
+				for (let i = 0; i < provinces.length; i ++)
+				{
+					place_is_choose[provinces[i]] = true
+				}
+				place_is_choose["湖北"] = false
+			}
+			else {
+				if (place_is_choose[d]){
+					place_is_choose[d] = false
+				}
+				else{
+					place_is_choose[d] = true
+				}
+				let only_hubei = true;
+				let except_hubei = true;
+				let whole_country = true;
+				for (let i = 0; i < provinces.length; i ++){
+					let current_province = provinces[i]
+					if (!place_is_choose[current_province])
+						whole_country = false
+					if (current_province === "湖北"){
+						if (place_is_choose[current_province])
+							except_hubei = false
+						else
+							only_hubei = false
+					}
+					else {
+						if (place_is_choose[current_province])
+							only_hubei = false
+						else
+							except_hubei = false
+					}
+				}
+				place_is_choose["除湖北"] = except_hubei
+				place_is_choose["全国"] = whole_country
+				place_is_choose["仅湖北"] = only_hubei
+
+			}
+			province_button.classed("selected_area_button", d => place_is_choose[d])
+			panel.reload_time()
+		})
+
+
+}
+
+
+// let ncpdata = new NCPdata()
+let panel
+let NCPdata = function(){
+  url = "https://tanshaocong.github.io/2019-nCoV/map.csv"
+  d3.csv(url, function(error, original_data){
+  		let places = ["全国", "仅湖北", "除湖北", "新疆", "西藏", "内蒙古", "青海", "四川", "黑龙江", "甘肃", "湖北", "云南", "广西", "湖南", "陕西", "广东", "吉林", "河北", "贵州", "山东", "江西", "河南", "辽宁", "山西", "安徽", "福建", "浙江", "江苏", "重庆", "宁夏", "海南", "台湾", "北京", "天津", "上海", "香港", "澳门"]
+		let provinces = ["新疆", "西藏", "内蒙古", "青海", "四川", "黑龙江", "甘肃", "湖北", "云南", "广西", "湖南", "陕西", "广东", "吉林", "河北", "贵州", "山东", "江西", "河南", "辽宁", "山西", "安徽", "福建", "浙江", "江苏", "重庆", "宁夏", "海南", "台湾", "北京", "天津", "上海", "香港", "澳门"]
+		// console.log(original_data)
+		data = get_modify_data(original_data)
+		window._data = data
+		console.log(data)
+		panel = new DataPanel(data, provinces)
+		panel.load_title()
+		panel.load_time()
+		panel.load_range(places)
+
+		// 获得时间范围：
+		console.log(panel.date_range) // 从第一天开始为0.
+		console.log(panel.data.begin) // 2020年1月1日为1
+
+		// 获得空间范围：
+		console.log(panel.place_is_choose) // 一个字典，存放着各个省份是否被选中。
+
+    })
+}
+
+// function update_data_selection(){
+// 	console.log()
+// }
+// console.log(data)
+NCPdata()
+
